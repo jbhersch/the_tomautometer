1f385b193b4d5955a92e02d76eb144974a9aaf9f
Jonathan Hersch <jbhersch@gmail.com>
2017-03-09 12:49:51 -0700

    commented rt_scrape.py

--- src/rt_scrape.py
+++ src/rt_scrape.py
@@ -10,24 +10,48 @@ rt_scrape.py - module containing methods to scrape the Rotten Tomatoes website
 f = lambda x: 1 if x=='fresh' else 0
 
 def get_review_urls(movie_name):
+    '''
+    INPUT:
+        - movie_name: name of movie of interest (string)
+    OUTPUT:
+        - review_urls: list of all review urls for the movie of interest
+    '''
+    # set the base review page url for the given movie
     url = "https://www.rottentomatoes.com/m/{0}/reviews/?page=1&sort=".format(movie_name)
+    # initiate beautiful soup
     response = get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    # get the total number of review pages
     num_pages = int(soup.select('span.pageInfo')[0].text.split()[-1])
 
+    # instantiate the review_urls list
     review_urls = []
+    # loop through the number of pages and append each page's url to the list
     for page in xrange(num_pages):
         review_urls.append("https://www.rottentomatoes.com/m/{0}/reviews/?page={1}&sort=".format(movie_name, page+1))
     return review_urls
 
 def get_reviews_and_labels(url):
+    '''
+    INPUT:
+        - url: url for review page of interest (string)
+    OUTPUT:
+        - reviews: list of reviews shown on given url (string list)
+        - labels: list of labels for reviews
+    '''
+
+    # initiate beautiful soup
     response = get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
+    # get the html tags for the reviews
     review_tags = soup.select('div.the_review')
+    # get the html tags for the review label icons
     icon_tags = soup.select('div.review_icon')
+    # instantiate the reviews and labels lists
     reviews = []
     labels = []
+    # loop through all reviews and append reviews and labels
     for n in xrange(len(review_tags)):
         if review_tags[n].text != " ":
             reviews.append(review_tags[n].text)
@@ -36,13 +60,23 @@ def get_reviews_and_labels(url):
     return reviews, labels
 
 def scrape_movie(movie_name):
+    '''
+    INPUT:
+        - movie_name: name of movie to scrape from rotten tomatoes (string)
+    OUTPUT:
+        - Tomato object containing movie title, reviews, and labels
+    '''
+    # get the urls for all the review pages of the movie
     urls = get_review_urls(movie_name)
+    # instantiate reviews, and labels lists
     reviews, labels = [], []
+    # loop through each review url
     for url in urls:
+        # scrape url for reviews and labels
         r, l = get_reviews_and_labels(url)
+        # extend reviews and labels lists
         reviews.extend(r)
-        labels.extend(l)
-    # return reviews, labels
+        labels.extend(l)    
     return Tomato(movie_name, reviews, labels)
 
 
